087aa116fc152d2fff2cc792eafc1772d436fce0
Juni0rFerreira <junioredilsonferreira@hotmail.com>
2024-04-21 22:36:55 -0300

    commit novos produtos casamento

--- src/bases/dadosCasamento.js
+++ src/bases/dadosCasamento.js
@@ -2,6 +2,15 @@ import conviterayssa from '../assets/imgs/casamentos/conviterayssa.jpeg';
 import votoscasamentorayssa from '../assets/imgs/casamentos/votoscasamentorayssa.jpeg';
 import menutriangularrayssa from '../assets/imgs/casamentos/menutriangularrayssa.jpg';
 import convitecamille from '../assets/imgs/casamentos/convitecamille.jpeg';
+import conviteindividualcamille from '../assets/imgs/casamentos/conviteindividualcamille.jpg';
+import convitemariana from '../assets/imgs/casamentos/convitemariana.jpg';
+import listapresentemariana from '../assets/imgs/casamentos/listapresentemariana.jpg';
+import listapresentevitoria from '../assets/imgs/casamentos/listapresentevitoria.jpg';
+import convitevitoria from '../assets/imgs/casamentos/convitevitoria.jpg';
+import convitevitoria2 from '../assets/imgs/casamentos/convitevitoria2.jpg';
+import manualdospadrinhos from '../assets/imgs/casamentos/manualdospadrinhos.jpg';
+import caixadoisbombons from '../assets/imgs/casamentos/caixadoisbombons.jpg';
+import caixapadrinhoscamille from '../assets/imgs/casamentos/caixapadrinhoscamille.jpg';
 
 const casamentos = [
 
@@ -28,6 +37,60 @@ const casamentos = [
     descricao: "17x21",
     valor: 2.00,
     imagem: convitecamille,
+  },
+  {
+    nome: "Convite Individual",
+    descricao: "3x5",
+    valor: 0.20,
+    imagem: conviteindividualcamille,
+  },
+  {
+    nome: "Convite Mariana",
+    descricao: "14x21",
+    valor: 3.00,
+    imagem: convitemariana,
+  },
+  {
+    nome: "Lista de Presente",
+    descricao: "3x5",
+    valor: 0.20,
+    imagem: listapresentemariana,
+  },
+  {
+    nome: "Lista de Presente c/ QRCode",
+    descricao: "4x6.5",
+    valor: 0.30,
+    imagem: listapresentevitoria,
+  },
+  {
+    nome: "Convite Vitoria",
+    descricao: "14x21",
+    valor: 2.50,
+    imagem: convitevitoria,
+  },
+  {
+    nome: "Convite Vitoria 2.0",
+    descricao: "14x21",
+    valor: 3.50,
+    imagem: convitevitoria2,
+  },
+  {
+    nome: "Manual dos Padrinhos",
+    descricao: "10x10",
+    valor: 7.00,
+    imagem: manualdospadrinhos,
+  },
+  {
+    nome: "Caixa 2 Bombons",
+    descricao: "18.5x21",
+    valor: 15.00,
+    imagem: caixadoisbombons,
+  },
+  {
+    nome: "Caixa Padrinhos",
+    descricao: "18.5x21",
+    valor: 20.00,
+    imagem: caixapadrinhoscamille,
   },
   
 ];
