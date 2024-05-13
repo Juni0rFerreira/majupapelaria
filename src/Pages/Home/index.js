671af7dd32ff6f64186e1f195145a14f16a05546
Juni0rFerreira <junioredilsonferreira@hotmail.com>
2024-05-12 22:35:31 -0300

    commit

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,7 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Header from '../../Components/Header';
-import { format } from 'date-fns-tz'; // Importando a função format do date-fns-tz
-import diadasmaesevento from '../../assets/imgs/diadasmaesevento.png'
 import './index.css';
 
 function HomePage() {
@@ -14,74 +12,10 @@ function HomePage() {
     window.open(urlWhatsApp, '_blank');
   };
 
-  // Calculando a data alvo (30/06/2024 às 18:00) no fuso horário de Brasília
-  const targetDate = new Date("2024-05-12T18:00:00-03:00");
-
-  const calculateTimeRemaining = () => {
-    // Obtendo a data atual no fuso horário de Brasília
-    const currentDateTime = new Date();
-    // Formatando a data atual no formato brasileiro (dd/MM/yyyy HH:mm:ss)
-    const currentDateTimeBR = format(currentDateTime, 'dd/MM/yyyy HH:mm:ss', { timeZone: 'America/Sao_Paulo' });
-    // Calculando a diferença em milissegundos
-    const difference = targetDate - currentDateTime;
-    // Convertendo a diferença para dias, horas, minutos e segundos
-    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
-    return { days, hours, minutes, seconds, currentDateTimeBR };
-  }
-
-  const [timeRemaining, setTimeRemaining] = useState(calculateTimeRemaining());
-  const [showCopacabana, setShowCopacabana] = useState(false);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      const remainingTime = calculateTimeRemaining();
-      setTimeRemaining(remainingTime);
-
-      // Verificando se chegou a zero segundos
-      if (remainingTime.days === 0 && remainingTime.hours === 0 && remainingTime.minutes === 0 && remainingTime.seconds === 0) {
-        setShowCopacabana(true);
-      }
-    }, 1000);
-
-    // Limpando o timer ao desmontar o componente
-    return () => clearTimeout(timer);
-  }, [timeRemaining]);
-
-
   return (
     <div className="App">
       <Header />
       <div>
-
-        <img
-          style={{
-            height: 'auto', // Mantém a proporção da imagem
-            maxWidth: '100%', // Garante que a imagem não ultrapasse a largura do contêiner
-            border: '8px'
-          }}
-          src={diadasmaesevento}
-          alt="Dia das Mães imagem"
-        />
-
-        <br />
-        <div className='timer'>
-          {showCopacabana ? (
-            <span>Dia das Mães Chegou!</span>
-          ) : (
-
-            <>
-              <span>{timeRemaining.days}d </span>
-              <span>{timeRemaining.hours}h </span>
-              <span>{timeRemaining.minutes}m </span>
-              <span>{timeRemaining.seconds}s </span>
-            </>
-          )}
-
-        </div>
-
         <br />
       </div>
       <div className='contato'>
--- src/bases/dadosAniversarios.js
+++ src/bases/dadosAniversarios.js
@@ -5,6 +5,17 @@ import caixabalamickeyeminie from '../assets/imgs/aniversarios/caixabalamickeyem
 import caixacubomickeyeminie from '../assets/imgs/aniversarios/caixacubomickeyeminie.jpeg'
 import caixacomalcamickeyeminie from '../assets/imgs/aniversarios/caixacomalcamickeyeminie.jpeg'
 import kitfestamickeyeminie from '../assets/imgs/aniversarios/kitfestamickeyeminie.jpg'
+import kitopcaodoiscinderella from '../assets/imgs/aniversarios/kitopcaodoiscinderella.jpeg'
+import caixameiabalacinderella from '../assets/imgs/aniversarios/caixameiabalacinderella.jpeg'
+import caixatravesseirocinderella from '../assets/imgs/aniversarios/caixatravesseirocinderella.jpeg'
+import caixabisduplocinderella from '../assets/imgs/aniversarios/caixabisduplocinderella.jpeg'
+import caixasushicinderella from '../assets/imgs/aniversarios/caixasushicinderella.jpeg'
+import caixacubocinderella from '../assets/imgs/aniversarios/caixacubocinderella.jpeg'
+import caixapiramidecinderella from '../assets/imgs/aniversarios/caixapiramidecinderella.jpeg'
+import centromesacinderella from '../assets/imgs/aniversarios/centromesacinderella.jpeg'
+import maletacinderella from '../assets/imgs/aniversarios/maletacinderella.jpeg'
+import caixapenteadeiracinderella from '../assets/imgs/aniversarios/caixapenteadeiracinderella.jpeg'
+import caixamilkcinderella from '../assets/imgs/aniversarios/caixamilkcinderella.jpeg'
 
 
 const aniversarios = [
@@ -48,8 +59,74 @@ const aniversarios = [
   {
     nome: "Kit Festa Opção 1",
     descricao: "12 unidades de cada",
-    valor: 259.99 ,
+    valor: 259.99,
     imagem: kitfestamickeyeminie,
+  },
+  {
+    nome: "Kit Festa Opção 2",
+    descricao: "100 unidades",
+    valor: 549.99,
+    imagem: kitopcaodoiscinderella,
+  },
+  {
+    nome: "Caixa Meia Bala Cinderella",
+    descricao: "9x4.7",
+    valor: 4.50,
+    imagem: caixameiabalacinderella,
+  },
+  {
+    nome: "Caixa Travesseiro Cinderella",
+    descricao: "8.5x9.3",
+    valor: 2.99,
+    imagem: caixatravesseirocinderella,
+  },
+  {
+    nome: "Caixa Bis Duplo Cinderella",
+    descricao: "6.5x5.5",
+    valor: 2.99,
+    imagem: caixabisduplocinderella,
+  },
+  {
+    nome: "Caixa Sushi Cinderella",
+    descricao: "7x7.5",
+    valor: 4.50,
+    imagem: caixasushicinderella,
+  },
+  {
+    nome: "Caixa Cubo Cinderella",
+    descricao: "6x6",
+    valor: 4.50,
+    imagem: caixacubocinderella,
+  },
+  {
+    nome: "Caixa Piramide Cinderella",
+    descricao: "11.5x6",
+    valor: 4.50,
+    imagem: caixapiramidecinderella,
+  },
+  {
+    nome: "Centro de Mesa Cinderella",
+    descricao: "29x11",
+    valor: 7.99,
+    imagem: centromesacinderella,
+  },
+  {
+    nome: "Maleta Cinderella",
+    descricao: "12x8.4",
+    valor: 4.50,
+    imagem: maletacinderella,
+  },
+  {
+    nome: "Caixa Penteadeira Cinderella",
+    descricao: "4.7x4.7",
+    valor: 2.99,
+    imagem: caixapenteadeiracinderella,
+  },
+  {
+    nome: "Caixa Milk Cinderella",
+    descricao: "12x6.2",
+    valor: 4.50,
+    imagem: caixamilkcinderella,
   },
   
 ];
